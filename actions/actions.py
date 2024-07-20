c96448e1377286ac7059a539d7a1b29b1e5e52e2
julihocc <juliho.castillo@outlook.com>
2024-07-20 08:58:43 -0600

    nlu intents formatted to standard labels

--- actions/actions.py
+++ actions/actions.py
@@ -1,27 +1,87 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+from datetime import datetime, timedelta
+from rasa_sdk import Action
 
 
-# This is a simple example for a custom action which utters "Hello World!"
+def text_date_to_int(text_date):
+    if text_date == "mañana":
+        return 1
+    if text_date == "ayer":
+        return -1
+    if text_date == "hoy":
+        return 0
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+
+weekday_mapping = [
+    "lunes",
+    "martes",
+    "miércoles",
+    "jueves",
+    "viernes",
+    "sábado",
+    "domingo"
+]
+
+
+def weekday_to_text(weekday: int):
+    return weekday_mapping[weekday]
+
+
+def display_date_support_error(text_date):
+    return f"""El sistema actualmente no puede procesar 
+la consulta de la fecha para {text_date}"""
+
+
+class ActionQueryTime(Action):
+    def name(self):
+        return "action_query_time"
+
+    def run(self, dispatcher, tracker, domain):
+        fecha = datetime.now()
+        hora = fecha.strftime("%H:%M:%S")
+        dispatcher.utter_message("Son las " + hora)
+        return []
+    
+    
+class ActionQueryDate(Action):
+    def name(self):
+        return "action_query_date"
+    
+    def run(self, dispatcher, tracker, domain):
+        text_date = tracker.get_slot("fecha") or "hoy"
+        int_date = text_date_to_int(text_date)
+        if int_date is not None: 
+            delta = timedelta(days=int_date)
+            current_date = datetime.now()
+            target_date = current_date + delta
+            dispatcher.utter_message(
+                text=target_date.strftime("Es %A %d de %B de %Y")
+            )
+        else:
+            dispatcher.utter_message(
+                display_date_support_error(text_date)
+            )
+        
+        return []
+                        
+
+class ActionQueryWeekday(Action):
+    def name(self):
+        return "action_query_weekday"
+    
+    def run(self, dispatcher, tracker, domain):
+        text_date = tracker.get_slot("fecha") or "hoy"
+        int_date = text_date_to_int(text_date)
+        if int_date is not None: 
+            delta = timedelta(days=int_date)
+            current_date = datetime.now()
+            target_date = current_date + delta
+            dispatcher.utter_message(
+                text=weekday_to_text(target_date.weekday())
+            )
+        else:
+            dispatcher.utter_message(
+                text=display_date_support_error(text_date)
+            )
+        return []
+    
+    
